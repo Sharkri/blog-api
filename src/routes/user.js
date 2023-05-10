7b89f56dfeb9fc5132ce43d9d2235256ee73fe93
Sharkri <sharkdude955@gmail.com>
2023-05-10 16:13:53 -0500

    Save user to db and return token as JSON

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import { body, validationResult, checkSchema } from "express-validator";
 import asyncHandler from "express-async-handler";
-import multer from "multer";
+import multer, { MulterError } from "multer";
 import User from "../../models/User";
 
 const router = Router();
@@ -15,30 +16,51 @@ const upload = multer({
     if (file.mimetype.startsWith("image/")) {
       cb(null, true);
     } else {
-      cb(new Error("File format should be an image"), false);
+      const error = new MulterError(400, "pfp");
+      error.message = "File format should be an image";
+
+      cb(error);
     }
   },
 });
 
-// Custom middleware function to map multer errors to express-validator errors
-const mapMulterErrorToValidationError = async (err, req, res, next) => {
-  if (err instanceof multer.MulterError) {
-    await checkSchema({
-      [err.field]: {
-        custom: {
-          errorMessage: err.message,
-        },
-      },
-    }).run(req);
-    next();
-  } else {
-    next(err);
-  }
-};
+const signToken = async (payload) =>
+  new Promise((resolve, reject) => {
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN },
+      (err, token) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(token);
+        }
+      }
+    );
+  });
+
+const uploadPfp = upload.single("pfp");
 
 router.post("/register", [
-  upload.single("pfp"),
-  mapMulterErrorToValidationError,
+  // upload pfp using multer
+  asyncHandler((req, res, next) => {
+    uploadPfp(req, res, async (err) => {
+      // Map multer errors to express-validator errors
+      if (err instanceof MulterError) {
+        await checkSchema({
+          [err.field]: {
+            custom: { errorMessage: err.message },
+          },
+        }).run(req);
+
+        next();
+      } else {
+        // unknown error
+        next(err);
+      }
+    });
+  }),
 
   body("email")
     .toLowerCase()
@@ -60,14 +82,32 @@ router.post("/register", [
     .isLength({ min: 1 })
     .withMessage("Display name is required"),
 
-  asyncHandler(async (req, res) => {
+  asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
-    // if there are errors
+
+    const { email, displayName, password } = req.body;
+    const pfp = req.file;
+
+    // if there are any errors
     if (!errors.isEmpty()) {
       res.status(400).json({ errors: errors.array() });
-    } else {
-      res.send("you made it!!");
+      return;
     }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      email,
+      displayName,
+      password: hashedPassword,
+      pfp: pfp
+        ? { data: req.file.buffer, contentType: req.file.mimetype }
+        : undefined,
+    });
+
+    await user.save();
+
+    const token = await signToken({ user });
+    res.json(token);
   }),
 ]);
 
