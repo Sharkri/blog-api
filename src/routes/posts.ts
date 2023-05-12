c0c3cc52ab8340008a91ee6efb34a3c8b832efb9
Sharkri <sharkdude955@gmail.com>
2023-05-12 16:15:24 -0500

    Fix deleting post logic

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -94,47 +94,21 @@ router.post(
 
 // --- DELETE POST ROUTE --- //
 
-router.post(
+router.delete(
   "/:postId/delete",
-  // checks that jwt token is authenticated and sets req.user
   verifyTokenAndGetUser,
 
-  ...getPostValidation(),
   asyncHandler(async (req, res) => {
     const { postId } = req.params;
 
-    if (!isValidObjectId(postId)) {
-      res.status(400).send("Invalid post id");
-      return;
-    }
-
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      res.status(400).json({ errors: errors.array() });
-      return;
-    }
-
     const post = await Post.findById<IPost>(postId, "author").exec();
 
-    if (post == null) res.status(404).send("Post not found");
+    if (!isValidObjectId(postId)) res.status(400).send("Invalid post id");
+    else if (post == null) res.status(404).send("404: Post not found");
     else if (!post.author.equals(req.user._id)) res.sendStatus(403);
     else {
-      const { title, description, blogContents, topics, isPublished } =
-        req.body;
-
-      const updatedPost = new Post<IPost>({
-        author: req.user._id,
-        title,
-        description,
-        blogContents,
-        topics,
-        isPublished,
-        _id: post._id, // required or else post will be assigned a new _id
-      });
-
-      await Post.findByIdAndUpdate(postId, updatedPost).exec();
-      res.json(updatedPost);
+      await Post.findByIdAndDelete(postId).exec();
+      res.json(post);
     }
   })
 );
